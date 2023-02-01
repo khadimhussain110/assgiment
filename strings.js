f3b6c55e3187999f1217822c202f3649058f1d99
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-02-01 13:01:57 -0800

    Add files via upload
    js assgiment

---
+++ strings.js
@@ -0,0 +1,134 @@
+//  chpter 2
+
+//  question 1
+// var username 
+
+// question 2
+
+// var myName;
+// var myName = "Gohar abbas";
+
+
+// question 3
+// var massage =("Hello Word");
+// alert(massage);
+
+// question 4
+// var biodata = ("gohar abbas \n 20 years old \n ceritified mobile app;ication develoment");
+// swal(biodata);
+
+// question 5
+// var pizza =("pizza \n pizz \n piz \n pi \n p");
+// swal(pizza);
+
+// question 6
+// var c = ("my enail address is example@exple.com");
+// swal(c);
+
+
+// question 7
+
+// var d = ("i am typing to learn from the Book A smarter way to learn javascript");
+// alert(d);
+
+
+// question 8
+
+// var f = ("Yah! I can write HTML content through Javascript");
+// document.write(f);
+
+
+// question 9
+
+// var e = (",____________%&___________,");
+// alert(e);
+
+
+
+// chpter 3
+
+// question 1
+
+// var age = ("i am 15 years old");
+// alert(age);
+
+
+// question 2
+
+// var b = ("you have visited this site 14 times");
+// alert(b);
+
+// question 3
+
+// var birthYou = ("My birth year is 1990 \n Data type of my ddeclared is number");
+// document.write(birthYou);
+
+// question 4 concept not clear
+
+
+
+
+
+// chapter 4
+
+
+
+
+
+
+// chapter 5
+
+// //  question 1 subtration
+// var num1 =(3);
+
+// var num2 =(5);
+
+// document.write(num1 + num2);
+
+//  multiplication
+// var num1 =(3);
+
+// var num2 =(5);
+
+// document.write(num1 * num2);
+
+//  division
+// var num1 =(3);
+
+// var num2 =(5);
+
+// document.write(num1 / num2);
+
+//  molulus
+// var num1 =(3);
+
+// var num2 =(5);
+// document.write(num1 % num2);
+
+
+// question 3
+// var ali;
+// document.write(ali);
+
+// question 4
+// var cost =("total cost buy 5 tickets to a movie is 3000PKR")
+// document.write(cost);
+
+// question 10
+
+// sum
+// var num1 = 2 +2;
+// document.write(num1);
+
+// multiply
+// var num2 = 10 * 14;
+// document.write(num2);
+
+// Divide
+// var num3 = 12 / 14;
+// document.write(num3);
+
+// question 11
+
+var age =("current year 2016 \n birth your 1993")
+document.write(age);
